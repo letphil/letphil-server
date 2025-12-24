00cef85880317bff68bb74f72b3f8bf37040c5f0
Choi Phil <philchoi@icloud.com>
2025-12-24 10:05:43 +0900

    adding some list services command need to work on remove-service

--- src/core/registry.ts
+++ src/core/registry.ts
@@ -1,11 +1,7 @@
 import appRegistry from './app.registry';
 
 // ---------------- Services Import --------------------
-import Users from '../services/users/users.controller';
-import Items from '../services/items/items.controller';
 
 // ------------- Services Registration -----------------
-appRegistry.register('users', Users);
-appRegistry.register('items', Items);
 
 export default appRegistry;
